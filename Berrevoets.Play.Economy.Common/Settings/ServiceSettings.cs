31fb34d0510f59bd4ce22521cee4215664724d1d
B.N. Berrevoets <bert@berrevoets.net>
2024-11-08 15:00:59 +0100

    Update project to .NET 8, add MongoDB support, new README
    Updated target framework to .NET 8.0 and package version to 1.0.0.
    Changed PackageReadmeFile property to readme.md. Added new package
    references for MongoDB and Microsoft.Extensions libraries. Added
    IEntity and IRepository<T> interfaces for CRUD operations. Added
    MongoDB integration with new extension methods and MongoRepository<T>
    class. Completely rewrote README.md to describe new library for
    monitored background services. Added ServiceSettings class.

---
+++ Berrevoets.Play.Economy.Common/Settings/ServiceSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Berrevoets.Play.Economy.Common.Settings;
+
+public class ServiceSettings
+{
+    public string DatabaseName   { get; init; }
+    public string CollectionName { get; init; }
+}
